19ca6a1aff8136b3dff7d0dd8b913e944fe78a10
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-10-29 17:05:18 +0100

    Add db-populate and db-read scripts for expense management

---
+++ backend/db-read.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { PrismaClient } from './generated/prisma/client.ts';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const expenses = await prisma.expense.findMany();
+  console.log(expenses);
+}
+
+main()
+  .finally(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  });
