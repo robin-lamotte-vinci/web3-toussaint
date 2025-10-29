ea6a10ace0bb99f71e3f60f5408089db57b0e6b0
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-10-29 17:05:38 +0100

    Refactor expenses service to use Prisma for database operations and remove file-based expense management

--- backend/services/expenses.js
+++ backend/services/expenses.js
@@ -1,27 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+import "dotenv/config";
+import { PrismaClient } from './generated/prisma/client.ts';
 
-const EXPENSES_FILE_PATH = path.join(__dirname, '../data/expenses.json');
-const EXPENSES_INIT_FILE_PATH = path.join(__dirname, '../data/expenses.init.json');
+const prisma = new PrismaClient();
+
+const queryDatabaseAndDisconnect = (operation) => {
+  return operation().finally(() => {
+    prisma.$disconnect();
+  });
+};
 
 function getAllExpenses() {
-  const data = fs.readFileSync(EXPENSES_FILE_PATH, 'utf8');
-  return JSON.parse(data);
+  const operation = () => prisma.expense.findMany();
+  const expenses = queryDatabaseAndDisconnect(operation);
+  console.log(expenses);
+  return expenses;
 }
 
 function addExpense(expense) {
-  const expenses = getAllExpenses();
-  expenses.push(expense);
-
-  const updatedExpenses = JSON.stringify(expenses, null, 2);
-  fs.writeFileSync(EXPENSES_FILE_PATH, updatedExpenses);
-  return expense;
+  const operation = () => prisma.expense.create({data: expense});
+  return queryDatabaseAndDisconnect(operation);
 }
 
+const INITIAL_DATA = [
+  { date: '2025-01-16T00:00:00Z', description: 'Example expense #1 from Alice', payer: 'Alice', amount: 25.5 },
+  { date: '2025-01-15T00:00:00Z', description: 'Example expense #2 from Bob', payer: 'Bob', amount: 35 },
+  { date: '2025-01-15T00:00:00Z', description: 'Example expense #3 from Alice', payer: 'Alice', amount: 2 },
+];
+
 function resetExpenses() {
-  const initData = fs.readFileSync(EXPENSES_INIT_FILE_PATH, 'utf8');
-  fs.writeFileSync(EXPENSES_FILE_PATH, initData);
-  return JSON.parse(initData);
+  const operation = async () => {
+    await prisma.expense.deleteMany({});
+    await prisma.expense.createMany({ data: INITIAL_DATA });
+    return prisma.expense.findMany();
+  };
+  return queryDatabaseAndDisconnect(operation);
 }
 
 module.exports = {
